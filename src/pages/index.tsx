7f90ce9a31e7c392359abc2e999f01db93ce6b56
fernandomarca <fernandomarca@hotmail.com>
2021-01-01 15:33:43 -0300

    stardart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,18 +5,17 @@ import { NextPage } from 'next'
 import RocketseatLogo from '../assets/rocketseat.svg'
 
 import { Container } from '../styles/pages/Home'
-const Home: NextPage = () => {
-  return (
-    <Container>
-      <Head>
-        <title>Homepage</title>
-      </Head>
 
-      <RocketseatLogo />
-      <h1>ReactJS Structure</h1>
-      <p>A ReactJS + Next.js structure made by Rocketseat.</p>
-    </Container>
-  )
-}
+const Home: NextPage = () => (
+  <Container>
+    <Head>
+      <title>Homepage</title>
+    </Head>
+
+    <RocketseatLogo />
+    <h1>ReactJS Structure</h1>
+    <p>A ReactJS + Next.js structure made by Rocketseat.</p>
+  </Container>
+)
 
 export default Home
